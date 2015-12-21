e3f48049347e37e05d3031322b58935e5bd9a0f3
SADA TAKAHASHI <sada.takahashi@gmail.com>
2015-12-21 22:51:33 +0900

    success to change string to json

--- web/server.rb
+++ web/server.rb
@@ -1,17 +1,19 @@
 #!/usr/local/bin/ruby
 
+require "JSON"
+
 FILE = "file.txt"
 text_ary = [] 
 #text_ary = Array.new
 File.open(FILE).each do |line|
+    line_hash = {}
     line.chomp!
-    text_ary << eval("{line.sub(/=/, ":")}")
-    #p line.split(":") 
-    #text_ary <<  line 
-end
+    #line.sub(/=/, ":")
+    line_hash = Hash[*(line.split("="))] 
+    text_ary <<  line_hash
+end 
+p text_ary
 
-puts text_ary
-
-
+puts JSON.generate(text_ary)
 
 
